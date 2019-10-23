fe1482cb7cb78a7a323dc298490e6981153a36b8
ghdus4185 <ckdghdus@naver.com>
2019-10-23 10:22:54 +0900

    수정된 user모델로 회원가입 로직 수정

--- accounts/views.py
+++ accounts/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as auth_login
 from django.contrib.auth import logout as auth_logout
+from .forms import CustomUserCreationForm
 # Create your views here.
+
+
 def signup(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('accounts:login')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     context = {
         'form': form
     }
     return render(request, 'accounts/form.html', context)
+
 
 def login(request):
     if request.method == "POST":
@@ -28,6 +32,7 @@ def login(request):
         'form': form
     }
     return render(request, 'accounts/form.html', context)
+
 
 def logout(request):
     auth_logout(request)
